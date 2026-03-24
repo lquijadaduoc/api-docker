6f716cca7517dee0e96c4da5592345e182812257
Luis Quijada <lquijada@duocuc.cl>
2026-03-24 20:46:34 -0300

    mejora

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import os
+import pandas as pd
+from sqlalchemy import create_engine, text
 from flask import Flask, jsonify
 
 app = Flask(__name__)
@@ -10,18 +13,35 @@ def ping():
 	}
     return jsonify(data)
 
-
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
 @app.route('/productos')
 def productos():
-    data = {
-	"code"  : 200,
-        "id"    : 1,
-        "name"  : "Martillo",
-	"price" : 5000,
-	"img"   : "https://d1nhio0ox7pgb.cloudfront.net/_img/g_collection_png/standard/512x512/hammer.png"
-        }
-  
-    return jsonify(data)
+    
+    query = "SELECT * FROM productos LIMIT 10"
+    
+    with engine.connect() as conn:
+        df = pd.read_sql(text(query), conn)
+
+    if not df.empty:
+        
+        lista_productos = []
+        
+        for _, row in df.iterrows():
+            item = {
+                "id": int(row['id']),
+                "name": row['nombre'], 
+                "price": float(row['precio'])
+            }
+            lista_productos.append(item)
+            
+        return jsonify({
+            "code": 200,
+            "total": len(lista_productos),
+            "data": lista_productos
+        })
+    
+    return jsonify({"code": 404, "message": "No se encontraron productos"}), 404
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
